c4ecdc56af5752d4923f43c08b9a5d7d232bdfe7
ND-alpha <twnethmidewmini@gmail.com>
2025-05-30 04:29:15 +0530

    done

---
+++ performance.php
@@ -0,0 +1,28 @@
+<?php include "st_home.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>Contact Us</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+  <style>
+    body {
+      background-color: #f8f9fa;
+    }
+    .main-content {
+        margin-left: 0;
+        margin-top: 60px;
+        padding: 20px;
+        transition: margin-left 0.3s ease;
+    }
+     .sidebar.active ~ .main-content {
+    margin-left: 250px;
+    }
+
+    </style>
+</head>
+<body>
+
+</body>
